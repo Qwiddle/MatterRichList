237ef51251e96ea62a50e39e0a09ba7a78fe865a
Andrew Rubenstein <andjruben@gmail.com>
2022-09-07 17:15:04 -0400

    create mapAccounts function
    Reduce fetched accounts and identify duplicates, then return a JS Map with the user address as the key. Map contains total rewards for all farms and identifying info on each farm.

--- src/index.js
+++ src/index.js
@@ -7,3 +7,52 @@ const fetchAccountsInternal = async () => {
   const res = await fetch(`${TZKT_API}/contracts/${MATTER}/bigmaps/accounts_internal/keys?limit=1000`);
   return res.json();
 }
+
+const mapAccounts = async () => {
+  const accounts = await fetchAccountsInternal();
+
+  const mapped = accounts.reduce((map, current) => {
+      const address = current.key.user_address;
+      const grouped = map.get(address);
+
+      current.totalReward = BigInt(current.value.reward);
+
+      if(!grouped) {
+        map.set(address, { 
+          userAddress: address,
+          totalReward: current.value.reward, 
+          farms: {
+            ...current.farms, 
+            [current.key.token.fa2_address]: { 
+              tokenId: current.key.token.token_id,
+              reward: BigInt(current.value.reward), 
+              staked: BigInt(current.value.staked)
+            }
+          }
+        });
+      } else {
+        map.set(address, { 
+          ...grouped, 
+          totalReward: BigInt(grouped.totalReward) + BigInt(current.totalReward), 
+          farms: {
+            ...grouped.farms, 
+            [current.key.token.fa2_address]: {
+              tokenId: current.key.token.token_id,
+              reward: BigInt(current.value.reward), 
+              staked: BigInt(current.value.staked)
+            }
+          }
+        });
+      }
+
+      return map;
+    }, new Map);
+
+  return mapped;
+}
+
+const start = async () => {
+  const accounts = await mapAccounts();
+}
+
+start();
