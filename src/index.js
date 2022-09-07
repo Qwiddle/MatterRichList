9c2a9d23f5feb786ccaa27aaab03b70e75989df5
Andrew Rubenstein <andjruben@gmail.com>
2022-09-07 15:58:08 -0400

    create fetchAccountsInternal function
    using TZKT api, return the relevant bigmap containing accounts that have received rewards/deposited into the matter smart contract

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
+import fetch from 'node-fetch';
+
 const TZKT_API = 'https://api.tzkt.io/v1';
 const MATTER = 'KT1K4jn23GonEmZot3pMGth7unnzZ6EaMVjY'; 
+
+const fetchAccountsInternal = async () => {
+  const res = await fetch(`${TZKT_API}/contracts/${MATTER}/bigmaps/accounts_internal/keys?limit=1000`);
+  return res.json();
+}
